f31d820733a70d4b9af08e7e5951fdd4ff3eb60a
mfarisgh <mfaris.official@gmail.com>
2024-06-30 21:58:22 +0800

    v5 - cucumber report

--- src/test/java/com/emeff23/stc/runner/RunCucumberTests.java
+++ src/test/java/com/emeff23/stc/runner/RunCucumberTests.java
@@ -3,8 +3,10 @@ package com.emeff23.stc.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "", features = {"src/test/resources/features/02-OrangeHRMSearchUsers.feature"}, glue = {"com.emeff23.stc.cucumbertests.definitions"},
-        plugin = {})
+@CucumberOptions(tags = "", features = {"src/test/resources/features/02-OrangeHRMSearchUsers.feature"},
+        glue = {"com.emeff23.stc.cucumbertests.definitions"},
+        plugin = { "pretty", "html:target/cucumber-reports.html" },
+        monochrome = true)
 
 public class RunCucumberTests extends AbstractTestNGCucumberTests{
 
